7af67744b0c3ba5172d8f68d85424f9168774de8
고연주 <koyj12061@naver.com>
2018-06-09 10:49:13 +0900

    챕터5 등록

---
+++ chapter 05/CatEscape/Assets/GameDirector.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameDirector : MonoBehaviour {
+
+public float hp = 0.1f;
+public Text timeText;
+GameObject hpGage;
+float elapsedTime;
+
+void Start() {
+	this.hpGage = GameObject.Find("hpGage");
+}
+	public void DecreaseHp() {
+		this.hpGage.GetComponent<Image> ().fillAmount -= hp;
+	}
+	
+	void Update()
+	{
+		elapsedTime += Time.deltaTime;
+		timeText.text = "시간:" + elapsedTime;
+	}
+}
